8f9de60ce35e8f8993f9000dbf9ba1e0f8d0ed62
Epa <epa@epaneto.com>
2015-04-20 01:50:34 -0300

    new content

--- app/js/data/waves.js
+++ app/js/data/waves.js
@@ -2,7 +2,7 @@ export default {
   "intro":  {
     "text": "The enemies have arrived to our village! You need to help us by crafting weapons before the waves come, so we can survive!",
     "countdown": 10,
-    "inventory": ["steel", "iron", "leather"],
+    "inventory": ["steel", "silver", "cloth"],
     "responses": {
       "long-sword": "great",
       "short-sword": "good"
@@ -20,7 +20,7 @@ export default {
       "inventory": ["iron", "copper", "leather"],
       "responses": {
         "king's-sword": "great",
-        "leathered-word": "good"
+        "leathered-sword ": "good"
       },
       "feedbacks": {
         "great": "The king's sword! That was a legend to be told!",
@@ -31,7 +31,7 @@ export default {
     {
       "text": "We are running out of resources. Be creative!",
       "countdown": 10,
-      "inventory": ["cotton", "copper", "leather"],
+      "inventory": ["cotton", "bronze", "fabric"],
       "responses": {
         "fiber-sword": "great",
         "light-sword": "good"
@@ -45,7 +45,7 @@ export default {
     {
       "text": "The boys brought what they've found in the barns... and the styes!",
       "countdown": 10,
-      "inventory": ["stone", "cotton", "mud"],
+      "inventory": ["stone", "silk", "mud"],
       "responses": {
         "stone-sword": "great",
         "rustic-sword": "good"
@@ -55,13 +55,179 @@ export default {
         "good": "A classic, yet very weird sword. Good enough!",
         "bad": "Are you trying to reach your inner nature?"
       }
+    },
+    {
+      "text": "The village masons came to help!",
+      "countdown": 10,
+      "inventory": ["wood", "stone", "sand"],
+      "responses": {
+        "wood-spear": "great",
+        "wooden-sword": "good"
+      },
+      "feedbacks": {
+        "great": "You spear-ed me some enemies. He he.",
+        "good": "Success! Not just for training boys, am I right?",
+        "bad": "Nice! We can't see sh*t!"
+      }
+    },
+    {
+      "text": "We are looting what we can from the village.",
+      "countdown": 10,
+      "inventory": ["mud", "wood", "dust"],
+      "responses": {
+        "double-wooden-sword": "great",
+        "earthen-sword": "good"
+      },
+      "feedbacks": {
+        "great": "A sword that can be swinged with both hands cuts deeper than fear!",
+        "good": "Great, smith! Interesting choice of material, though.",
+        "bad": "You know how many men slipped on the dirt? It's not funny!"
+      }
     }
   ],
   "medium": [
-
+    {
+      "text": "Our stash was robbed! This is everything I found to help you...",
+      "countdown": 10,
+      "inventory": ["cat", "boots", "straw"],
+      "responses": {
+        "battle-cat": "great",
+        "puss-in-boots ": "good"
+      },
+      "feedbacks": {
+        "great": "I hope you trained this cats for more battle cause the enemy couldn't take them!",
+        "good": "A puss in boots is more effective than swords! Who would say?",
+        "bad": "Pussy cats are no good in battle! Can I keep him?"
+      }
+    },
+    {
+      "text": "The enemies brought an arcane! Do we have anything useful?",
+      "countdown": 10,
+      "inventory": ["tissue", "candles", "brass"],
+      "responses": {
+        "fire-sword": "great",
+        "brass-sword": "good"
+      },
+      "feedbacks": {
+        "great": "Wow! Even arcanes couldn't stand a powerful fire sword!",
+        "good": "With those resources you kinda did the job!",
+        "bad": "What? Were you expecting to summon a phoenix?"
+      }
+    },
+    {
+      "text": "The enemies have battle dogs! We are doomed!",
+      "countdown": 10,
+      "inventory": ["ragdoll", "pepper", "lemon"],
+      "responses": {
+        "super-hot-doll": "great",
+        "hot-doll": "good"
+      },
+      "feedbacks": {
+        "great": "They will probably eat by a straw for the next month!",
+        "good": "I have never seen running hot dogs ba-ha-ha!",
+        "bad": "Can't say it isn't tasty but...really?!"
+      }
+    },
+    {
+      "text": "Bats! Bats everywhere!",
+      "countdown": 10,
+      "inventory": ["banana", "log", "apple"],
+      "responses": {
+        "torch": "great",
+        "fire": "good"
+      },
+      "feedbacks": {
+        "great": "Batman is running back home!",
+        "good": "The smoke dispersed those bats!",
+        "bad": "Would you like an orange juice, mr. bat?"
+      }
+    },
+    {
+      "text": "Damn, they have clerigs! The enemies are healing their wounded!",
+      "countdown": 10,
+      "inventory": ["pillow", "beer-jug", "pumpkin"],
+      "responses": {
+        "beer-hammer": "great",
+        "wet-pillow": "good"
+      },
+      "feedbacks": {
+        "great": "Ever heard the saying 'a clerig and a beer hammer, one's drunk and then slammed'? I just invented it!",
+        "good": "Hey, this thing hits really hard! Well done!",
+        "bad": "Were you inventing a new party game? I didn't get it."
+      }
+    }
   ],
   "hard": [
-
+    {
+      "text": "The enemies grew tired of making sense! They brought bunnies!",
+      "countdown": 10,
+      "inventory": ["carrots", "vodka", "chocolate", "powder"],
+      "responses": {
+        "explosive-vodka-carrots": "great",
+        "vodka-carrots": "good"
+      },
+      "feedbacks": {
+        "great": "Those bunnies will never see easter again!",
+        "good": "Drunk bunnies going home!",
+        "bad": "Great, now it's easter season forever..."
+      }
+    },
+    {
+      "text": "The enemies brought pretty witches to enchant our soldiers! We need to do something!",
+      "countdown": 10,
+      "inventory": ["potion", "perfume", "cigarrete", "salt"],
+      "responses": {
+        "ogre-potion": "great",
+        "smelly-potion": "good"
+      },
+      "feedbacks": {
+        "great": "I bet those ogres will find nothing to enchant from now on!",
+        "good": "Jesus, what a smell! What did you put there?",
+        "bad": "Of course, let's turn those beautiful witches into Miss Tavern MCDLXXI..."
+      }
+    },
+    {
+      "text": "A dragon! They have a dragon!!!!!!!",
+      "countdown": 10,
+      "inventory": ["gnome", "spoon", "spear", "gear"],
+      "responses": {
+        "super-battle-gnome": "great",
+        "battle-gnome": "good"
+      },
+      "feedbacks": {
+        "great": "A dragon with gnomophobia and a hyped up gnome! You're a genius, smith!",
+        "good": "This gnome is really brave, or that dragon is really a coward!",
+        "bad": "I know we lost, but the gnome is funny."
+      }
+    },
+    {
+      "text": "Their necromancer is reviving their fallen soldiers and turning them into zombies!",
+      "countdown": 10,
+      "inventory": ["boombox", "bass", "crystal-gloves", "rune"],
+      "responses": {
+        "thriller": "great",
+        "rhythm": "good"
+      },
+      "feedbacks": {
+        "great": "Cause this is thriller, thriller night, and no one's gonna save you from our smith that works so hard!",
+        "good": "Zombies gone dancing! The necromancer lost!",
+        "bad": "Yeah, gloves that walk alone, how nice."
+      }
+    },
+    {
+      "text": "What? The enemies have a Propeller Knight! How do we fight that?",
+      "countdown": 10,
+      "inventory": ["stick", "tavern-sign", "magic", "courage"],
+      "responses": {
+        "shovel-knight": "great",
+        "shovel": "good"
+      },
+      "feedbacks": {
+        "great": "O-M-G Shovel Knight came here only to help us! And left autographs!",
+        "good": "Why didn't we get a shovel before? It's brilliant!",
+        "bad": "How wonderful, a powerful wizard that does nothing because he has a STICK."
+      }
+    }
   ]
 }
 
